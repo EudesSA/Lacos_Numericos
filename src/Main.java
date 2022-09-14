b7c8d4d07ce3317328358d7dbfdf8e91d6e94f30
EudesAguiar <100742690+EudesAguiar@users.noreply.github.com>
2022-09-14 09:40:21 -0300

    Apredendo o comando FOR

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+        //incremento de 1 até 10
         for (int i = 1; i <= 10; i++){
             System.out.println(i);
+        }
+
+        System.out.println("\n \n");
+
+        //números impares
+        for (int a = 1; a <= 10; a+=2){
+                System.out.println(a);
+        }
+
+        System.out.println("\n \n");
+
+        //números pares
+        for (int b = 1; b <= 10; b+=1){
+            if (b % 2 == 0){
+            System.out.println(b);
+            }
         }
     }
 }
